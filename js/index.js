a63167eeb480ec61600ee6316671aa5e63ee07e8
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2020-12-10 15:03:04 -0300

    create js folder & file

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector ('.header_menu')
+const burgerButton = document.querySelector('#burger-menu')
+const ipad = window.matchMedia(' screen and (max-width: 767px)')
+ipad.addListener(validation)
+validation(ipad)
+const validation = e => {
+	if (e.matches) {
+		burgerButton.addEventListener('click', showHide)
+	} else {
+		burgerButton.removeEventListener('click', showHide)
+	}
+}
+const showHide = () => menu.classList.toggle('is-active')
