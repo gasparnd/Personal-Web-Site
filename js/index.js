6d325e97c83b8684a92e8daef2017d3889102f97
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-04-25 17:18:51 -0300

    website update, now in english

--- js/index.js
+++ js/index.js
@@ -7,27 +7,3 @@ const burgerBTN = document.querySelector('#burger-menu')
 const ipad = window.matchMedia(' screen and (max-width: 767px)')
 ipad.addListener(validation)
 validation(ipad)
-
-// SCREEN MODE
-
-const screenBTN = document.querySelector('.screen-mode')
-screenBTN.addEventListener('click', screenMode)
-function screenMode () {
-	const cssVar = document.querySelector(':root')
-	screenBTN.classList.toggle('light')
-	if (screenBTN.classList.contains('light')) {
-		cssVar.style.cssText = `--background-color: white;
-								--text-color: #24292e;
-								--background-color-2: #e1e4e8;
-								--border: #cfd3ce;
-								--tags-background-color: #cfd3ce;
-								--hover-background-color: #cfd3ce;`
-	} else {
-		cssVar.style.cssText = `--background-color: #010101;
-								--text-color: #c9d1d9;
-								--background-color-2: #0d1117;
-								--border: #30363d;
-								--tags-background-color: #33313b;
-								--hover-background-color:#222831;`
-	}
-}
